bb0c6fb263ea60c8fd6a65f72362e583a1d6f852
Julius W <juliuspanjul25@gmail.com>
2022-01-02 23:24:49 +0700

    Called checkProgram function and added descriptions for logs

--- ml.sh
+++ ml.sh
@@ -4,9 +4,14 @@
 
 APP_NAME="com.mobile.legends"
 
+## WARNING! ##
+# IF YOU CHANGED THE SCRIPT NAME, EDIT THIS LINE
+# TO MAKE THE PROGRAM WORK CORRECTLY!
+SCRIPT_NAME="ml"
+
 ### Program Variables ###
 
-## CAUTION! ##
+## WARNING!! ##
 # THIS EXT_SDCARD ASSIGNMENT WILL NOT WORK IF
 # THERE'S MORE THAN 1 EXTERNAL STORAGE CONNECTED!
 EXT_SDCARD=
@@ -26,6 +31,9 @@ if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
     echo "-h --help      Display this"
     echo "-d --display   Print stdout and stderr to terminal"
     echo "-n --nomount   Disable mount(for testing)"
+    echo
+    echo "https://github.com/jTnqr/Mobile-Legends-on-EXTSD"
+    echo "Created by jTnqr"
     exit
 else
     if [[ "$1" == "-d" ]] || [[ "$1" == "--display" ]]; then
@@ -46,10 +54,12 @@ fi
 
 function findEXTSD() {
     if [[ $EXT_SDCARD == "" ]]; then
-        echo "Variable is empty"
-        EXT_TEMP="$(ls -d /mnt/media_rw/* | head -n 1)" || exit 2
+        echo "ERROR: \$EXT_SDCARD is empty. Searching..."
+        EXT_TEMP="$(ls -d /mnt/media_rw/* | head -n 1)" || \
+        echo "ERROR: EXT_SDCARD is not found." && \
+        exit 2
         EXT_SDCARD="$EXT_TEMP"
-        sed -i -r "s#^(EXT_SDCARD=).*#\1$EXT_TEMP#" $SCRIPT_PATH/ml.sh
+        sed -i -r "s#^(EXT_SDCARD=).*#\1$EXT_TEMP#" $SCRIPT_PATH/$SCRIPT_NAME.sh
         exit
     fi
 }
@@ -58,7 +68,7 @@ function checkMount() {
     if [[ "$1" == "$EXT_SDCARD" ]]; then
         while ! mountpoint -q "$EXT_SDCARD"; do
             sleep 1
-        done && echo "SD card is mounted"
+        done && echo "INFO: EXT_SDCARD is mounted."
     elif [[ "$(awk '/fuse/ && /$1/ {print 1}' /proc/mounts)" == 1 ]]; then
         return 1
     fi
@@ -68,14 +78,14 @@ function checkMount() {
 function checkProgram() {
     if [ ! -f /system/bin/bindfs ]; then
         if [ ! -f /data/media/0/bindfs ]; then
-            echo "bindfs is not found!"
+            echo "ERROR: bindfs is not found on /sdcard!"
             exit
         fi
         
         if [ ! -w / ]; then
             RO_ROOT=1
             mount -o rw,remount / || \
-            echo "Cannot change / to r/w" && \
+            echo "ERROR: Cannot change / to r/w!" && \
             exit
         fi
         
@@ -88,9 +98,9 @@ function checkProgram() {
         fi
 
         if [ -f /system/bin/bindfs ]; then
-            echo "Successfully copied bindfs"
+            echo "SUCCESS: Successfully copied bindfs!"
         else
-            echo "Cannot copy bindfs"
+            echo "ERROR: Cannot copy bindfs!"
             exit
         fi
     fi
@@ -103,22 +113,23 @@ function moveFiles() {
 
 function mountML() {
     if [[ "$(checkMount $SD0_DIR)" == 1 ]]; then
-        echo "$APP_NAME is already mounted"
-        return 1
+        echo "ERROR: $APP_NAME is already mounted!"
+        exit
     else
         if [ "$(ls -A $SD0_DIR)" ]; then
-            echo "Moving files.."
+            echo "INFO: Moving files..."
             moveFiles
         fi
         bindfs -u $(stat -c %u /sdcard/Android/data/$APP_NAME) -g 9997 -p a-rwx,ug+rw,ug+X $SD1_DIR /mnt/runtime/write/emulated/0/Android/data/$APP_NAME && \
-        echo "$APP_NAME is mounted successfully" || \
-        echo "Failed to mount $APP_NAME"
+        echo "SUCCESS: $APP_NAME is mounted successfully!" || \
+        echo "ERROR: Failed to mount $APP_NAME!"
     fi
 }
 
 ### Main ###
 
 findEXTSD
+checkProgram
 checkMount $EXT_SDCARD
 
 if [[ $MOUNT_ML == 1 ]]; then
